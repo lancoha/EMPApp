41dacbc3480731e9597ea394d65599afddc79446
lancoha <lancoha@gmail.com>
2024-12-19 17:03:53 +0100

    PB2 - no internet chart(favourites)

--- app/src/main/java/com/example/empapp/MainActivity.kt
+++ app/src/main/java/com/example/empapp/MainActivity.kt
@@ -1,13 +1,10 @@
 package com.example.empapp
 
-import TwelveDataApi
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
